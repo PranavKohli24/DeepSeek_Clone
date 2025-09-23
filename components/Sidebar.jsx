376733f9e6fd3d8ec654d18509f4e72144875543
Pranav Kohli <kohlipranav24@gmail.com>
2025-09-24 03:41:15 +0530

    add chatlabel component

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,14 +1,17 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { useClerk, UserButton } from '@clerk/nextjs'
 import { useAppContext } from '@/context/AppContext'
+import ChatLabel from './ChatLabel'
 
 const Sidebar = ({expand,setExpand}) => {
 
     const {openSignIn}=useClerk();
 
     const {user}=useAppContext();
+
+    const [openMenu,setOpenMenu]=useState({id:0,open:false});
 
   return (
     <div className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand?'p-4 w-64':'md:w-20 w-0 max-md:overflow-hidden'}`}>
@@ -48,6 +51,7 @@ const Sidebar = ({expand,setExpand}) => {
             <p className='my-1'>Recents</p>
 
             {/* chatLabel */}
+            <ChatLabel openMenu={openMenu} setOpenMenu={setOpenMenu}/>
         </div>
       </div>
 
