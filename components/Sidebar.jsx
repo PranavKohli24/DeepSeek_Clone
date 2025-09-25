92f1e71bc075d5b3efbedf96210736e055b49540
Pranav Kohli <kohlipranav24@gmail.com>
2025-09-25 16:08:00 +0530

    add rename chat, delete chat feature

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -9,7 +9,7 @@ const Sidebar = ({expand,setExpand}) => {
 
     const {openSignIn}=useClerk();
 
-    const {user}=useAppContext();
+    const {user, chats, createNewChat}=useAppContext();
 
     const [openMenu,setOpenMenu]=useState({id:0,open:false});
 
@@ -34,7 +34,7 @@ const Sidebar = ({expand,setExpand}) => {
             </div>
         </div>
 
-        <button  className={`mt-8 flex items-center justify-center cursor-pointer 
+        <button onClick={createNewChat} className={`mt-8 flex items-center justify-center cursor-pointer 
             ${expand?'bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5 w-max':'group-relative h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg'}`}>
             <Image className={expand?'w-6':'w-7'} src={expand?assets.chat_icon:assets.chat_icon_dull} alt=""/>
             <div className='absolute w-max -top-12 -right-12 opacity-0 group-hover:opacity-100 transition bg-black
@@ -51,7 +51,8 @@ const Sidebar = ({expand,setExpand}) => {
             <p className='my-1'>Recents</p>
 
             {/* chatLabel */}
-            <ChatLabel openMenu={openMenu} setOpenMenu={setOpenMenu}/>
+            {chats.map((chat, index)=><ChatLabel key={index} name={chat.name} id={chat._id} openMenu={openMenu} setOpenMenu={setOpenMenu}/>)}
+            
         </div>
       </div>
 
